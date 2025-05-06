d4846699e46398f53af293980f04502691ad39ba
Nicolas Gatien <nicolas.gatien6@gmail.com>
2025-05-06 12:07:05 -0400

    ✍️ Rename Variables

--- main.py
+++ main.py
@@ -1,78 +1,81 @@
 import pylab
 import math
 
-vinit = 0.0
-dt = 0.01
-ena = 115
-gna = 120
-ek = -12
-gk = 36
-el = 10.6
-gl = 0.3
+INITIAL_VOLTAGE = 0.0
+TIMESTEP = 0.01
+e_SODIUM = 115
+SODIUM_CHANNEL_CONDUCTANCE = 120
+e_POTASSIUM = -12
+POTASSIUM_CHANNEL_CONDUCTANCE = 36
+e_LEAK_CURRENT = 10.6
+LEAK_CURRENT_CHANNEL_CONDUCTANCE = 0.3
 
-def upd(x, dlta_x):
-    return x + dlta_x * dt
+def update(x, dlta_x):
+    return x + dlta_x * TIMESTEP
 
 def mnh0(a, b):
     return a / (a + b)
 
-def am (v) : return (2.5 - 0.1 * v) / (math.exp(2.5 - 0.1 * v) - 1)
-def bm (v) : return 4 * math.exp((-1) * v / 18)
-def an (v) : return (0.1 - 0.01 * v) / (math.exp(1 - (0.1 * v)) - 1)
-def bn (v) : return 0.125 / math.exp((-1) * v / 80)
-def ah (v) : return 0.07 * math.exp((-1) * v / 20)
-def bh (v) : return 1 / (math.exp(3 - (0.1) * v) + 1)
+def am (voltage) : return (2.5 - 0.1 * voltage) / (math.exp(2.5 - 0.1 * voltage) - 1)
+def bm (voltage) : return 4 * math.exp((-1) * voltage / 18)
 
-m0 = mnh0(am(0), bm(0)) 
-n0 = mnh0(an(0), bn(0))
-h0 = mnh0(ah(0), bh(0))
+def an (voltage) : return (0.1 - 0.01 * voltage) / (math.exp(1 - (0.1 * voltage)) - 1)
+def bn (voltage) : return 0.125 / math.exp((-1) * voltage / 80)
 
-def ina(m, h, v):
-    return gna * (m ** 3) * h * (v - ena)
+def ah (voltage) : return 0.07 * math.exp((-1) * voltage / 20)
+def bh (voltage) : return 1 / (math.exp(3 - (0.1) * voltage) + 1)
 
-def ik(n, v):
-    return gk * (n ** 4) * (v - ek)
+m0 = mnh0(am(INITIAL_VOLTAGE), bm(INITIAL_VOLTAGE)) 
+n0 = mnh0(an(INITIAL_VOLTAGE), bn(INITIAL_VOLTAGE))
+h0 = mnh0(ah(INITIAL_VOLTAGE), bh(INITIAL_VOLTAGE))
 
-def il(v):
-    return gl * (v - el)
+def calculate_sodium(m, h, voltage):
+    return SODIUM_CHANNEL_CONDUCTANCE * (m ** 3) * h * (voltage - e_SODIUM)
 
-def newS(v, m, n, h, t):
-    if t < 5.0 or t > 6.0:
-        istim = 0.0
+def calculate_potassium(n, voltage):
+    return POTASSIUM_CHANNEL_CONDUCTANCE * (n ** 4) * (voltage - e_POTASSIUM)
+
+def calculate_leak_currents(voltage):
+    return LEAK_CURRENT_CHANNEL_CONDUCTANCE * (voltage - e_LEAK_CURRENT)
+
+def new_step(voltage, m, n, h, current_timestep):
+    if current_timestep < 5.0 or current_timestep > 6.0:
+        injected_stimulation = 0.0
     else:
-        istim = 20.0
-    dv = istim - (ina(m, h, v) + ik(n,v) + il(v))
-    dm = am(v) * (1 - m) - bm(v) * m
-    dn = an(v) * (1 - n) - bn(v) * n
-    dh = ah(v) * (1 - h) - bh(v) * h
-    vp = upd(v, dv)
-    tp = t + dt
-    mp = upd(m, dm)
-    np = upd(n, dn)
-    hp = upd(h, dh)
-    return (vp, mp, np, hp, tp)
+        injected_stimulation = 20.0
+    
+    d_voltage = injected_stimulation - (calculate_sodium(m, h, voltage) + calculate_potassium(n, voltage) + calculate_leak_currents(voltage))
+    dm = am(voltage) * (1 - m) - bm(voltage) * m
+    dn = an(voltage) * (1 - n) - bn(voltage) * n
+    dh = ah(voltage) * (1 - h) - bh(voltage) * h
+    voltage_p = update(voltage, d_voltage)
+    timestep_p = current_timestep + TIMESTEP
+    mp = update(m, dm)
+    np = update(n, dn)
+    hp = update(h, dh)
+    return (voltage_p, mp, np, hp, timestep_p)
 
-vs = []
+voltage_s = []
 ms = []
 ns = []
 hs = []
-ts = []
-a, b, c, d, e = newS(vinit, m0, n0, h0, 0.0)
-vs.append(a)
+timestep_s = []
+a, b, c, d, e = new_step(INITIAL_VOLTAGE, m0, n0, h0, 0.0)
+voltage_s.append(a)
 ms.append(b)
 ns.append(c)
 hs.append(d)
-ts.append(e)
+timestep_s.append(e)
 for i in (range(2, 3000)):
-    a, b, c, d, e = newS(vs[-1], ms[-1], ns[-1], hs[-1], ts[-1])
-    vs.append(a)
+    a, b, c, d, e = new_step(voltage_s[-1], ms[-1], ns[-1], hs[-1], timestep_s[-1])
+    voltage_s.append(a)
     ms.append(b)
     ns.append(c)
     hs.append(d)
-    ts.append(e) 
+    timestep_s.append(round(e, 2)) 
 
-pylab.plot(ts, ms)
-pylab.plot(ts, ns)
-pylab.plot(ts, hs)
+pylab.plot(timestep_s, ms)
+pylab.plot(timestep_s, ns)
+pylab.plot(timestep_s, hs)
 
 pylab.show()
